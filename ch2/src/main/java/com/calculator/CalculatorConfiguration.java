10db3b187d016d053cad02cd3f88cda274042241
Na Yoonho <lucky72.nyh@gmail.com>
2018-09-28 21:29:32 +0900

    ch2 진행 중

---
+++ ch2/src/main/java/com/calculator/CalculatorConfiguration.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by marten on 22-01-17.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan
+public class CalculatorConfiguration {
+}
