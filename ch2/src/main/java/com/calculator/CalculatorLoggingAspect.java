3b52dc62dc253a6d49080410795350549b336d2a
Na Yoonho <lucky72.nyh@gmail.com>
2018-10-05 22:22:21 +0900

    3-10

--- ch2/src/main/java/com/calculator/CalculatorLoggingAspect.java
+++ ch2/src/main/java/com/calculator/CalculatorLoggingAspect.java
@@ -1,5 +1,7 @@
 package com.calculator;
 
+import java.util.Arrays;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -7,57 +9,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 @Aspect
 @Component
 public class CalculatorLoggingAspect {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Before("CalculatorPointcuts.loggingOperation()")
-    public void logBefore(JoinPoint joinPoint) {
-        log.info("The method " + joinPoint.getSignature().getName()
-                + "() begins with " + Arrays.toString(joinPoint.getArgs()));
-    }
-
-    @After("CalculatorPointcuts.loggingOperation()")
-    public void logAfter(JoinPoint joinPoint) {
-        log.info("The method " + joinPoint.getSignature().getName()
-                + "() ends");
-    }
-
-    @AfterReturning(
-            pointcut = "CalculatorPointcuts.loggingOperation()",
-            returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        log.info("The method " + joinPoint.getSignature().getName()
-                + "() ends with " + result);
-    }
-
-    @AfterThrowing(
-            pointcut = "CalculatorPointcuts.loggingOperation()",
-            throwing = "e")
-    public void logAfterThrowing(JoinPoint joinPoint, IllegalArgumentException e) {
-        log.error("Illegal argument " + Arrays.toString(joinPoint.getArgs())
-                + " in " + joinPoint.getSignature().getName() + "()");
-    }
-
-    @Around("CalculatorPointcuts.loggingOperation()")
+    @Around("execution(* *.*(..))")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("The method " + joinPoint.getSignature().getName()
-                + "() begins with " + Arrays.toString(joinPoint.getArgs()));
+        log.info("The method {}() begins with {}", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
         try {
             Object result = joinPoint.proceed();
-            log.info("The method " + joinPoint.getSignature().getName()
-                    + "() ends with " + result);
-            return result;
+            log.info("The method {}() ends with {}", joinPoint.getSignature().getName(), result);
+
         } catch (IllegalArgumentException e) {
-            log.error("Illegal argument "
-                    + Arrays.toString(joinPoint.getArgs()) + " in "
-                    + joinPoint.getSignature().getName() + "()");
+            log.error("Illegal argument {} in {}()", Arrays.toString(joinPoint.getArgs()) , joinPoint.getSignature().getName());
             throw e;
         }
     }
+
 }
 
