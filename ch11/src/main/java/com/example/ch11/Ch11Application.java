fded64726984b0c555c00ed729a3e0359ecd4ed2
Na Yoonho <lucky72.nyh@gmail.com>
2018-11-23 20:46:16 +0900

    챕터15

---
+++ ch11/src/main/java/com/example/ch11/Ch11Application.java
@@ -0,0 +1,12 @@
+package com.example.ch11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch11Application.class, args);
+    }
+}
